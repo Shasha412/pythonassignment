c5fdeab3d02a65ef3d36dfdd1fccc16fb711bf68
Shasha412 <anishachd202@gmail.com>
2025-05-09 23:54:24 +0530

    Create m5.py

---
+++ m5.py
@@ -0,0 +1,9 @@
+# m5.py
+import cal1
+
+print("Add:", cal1.add1(10, 5))
+print("Subtract:", cal1.sub1(10, 5))
+print("Multiply:", cal1.mul1(10, 5))
+print("Divide:", cal1.div1(10, 0))
+print("Modulus:", cal1.mod1(10, 3))
+print("Power:", cal1.pow1(2, 3))
