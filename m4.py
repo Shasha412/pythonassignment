733eff38d36eb7f2b696e2e615a3be86b2f0e1c5
Shasha412 <anishachd202@gmail.com>
2025-05-09 23:50:35 +0530

    Create m4.py

---
+++ m4.py
@@ -0,0 +1,22 @@
+# m4.py
+
+import list1
+import set2
+import dict3
+
+# List operations
+list1.append1("Python")
+list1.extend1(["language", "programming"])
+list1.pop()
+list1.remove1("language")
+
+# Set operations
+set2.adds2(5)
+set2.remove2(3)
+set2.slen2(set2.myset)
+
+# Dict operations
+dict3.add3("A", 1)
+dict3.keys3()
+dict3.values3()
+dict3.len3(dict3.mydict)
