9cf23c9c8b99734135f299b2645ebc7dd2f4102b
Shasha412 <anishachd202@gmail.com>
2025-05-09 23:37:23 +0530

    Create nameerror.py

---
+++ nameerror.py
@@ -0,0 +1,11 @@
+# Make name error handler with built in exceptions
+
+x=9
+y=0
+try:
+    print(z)
+except NameError :
+    print("Error in name")
+except ValueError :
+    print("error in value")
+print(x)
